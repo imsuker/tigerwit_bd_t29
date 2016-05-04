534966f1a051ba9b5a5c2b6a9ead0e1df0023434
gukw <38353285@qq.com>
2016-05-04 19:05:49 +0800

    添加弹框

--- js/t28.js
+++ js/t28.js
@@ -2,18 +2,66 @@ $(function() {
     $(".hy-tabs-ul li").each(function(index){
         var liCurrent = $(this);
         var liIndex = index;
-
         liCurrent.click(function(){
             // console.info(index);
             liCurrent.addClass("hy-tabs-ul-active").siblings().removeClass("hy-tabs-ul-active");
             $(".hy-tabs-con .hy-tabs-con1:eq(" + liIndex +")").addClass("div-block").siblings().removeClass("div-block");
 
         });
-
     });
-
 });
 
-function info() {
-    alert("2016年5月5日开始报名");
+//方法获取cookie
+function getCookie(name)
+{
+  var arr,reg=new RegExp("(^| )"+name+"=([^;]*)(;|$)");
+  if(arr=document.cookie.match(reg)){
+    return unescape(arr[2]);
+  }
+  else{
+    return null;
+  }
 }
+function isLogin(){
+  var user_code = getCookie('user_code');
+  if(user_code){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+$(function(){
+  var dEnroll = $(".enroll");
+  dEnroll.click(function(){
+    if(!isLogin()){
+       toLogin();
+       return;
+    }else{
+      layer.load();
+      var game = $(this).attr('_type');
+      $.getScript('http://www.tigerwit.com/action/public/v3/apply_to_master',{
+        game : game
+      },function(){
+        layer.closeAll('loading');
+        layer.alert('报名成功', {
+            skin: 'layui-layer-molv' //样式类名
+            ,closeBtn: 0
+        }, function(){
+        });
+      });
+    }
+    var _type = $(this).attr('_type');
+  });
+  function toLogin(){
+    layer.confirm('需要登录后才可以报名', {
+        btn: ['登录','注册','暂不'] //按钮
+    }, function(){
+      location.href="https://www.tigerwit.com/space/#/account/login?back="+location.href;
+    }, function(){
+      location.href="https://www.tigerwit.com/space/#/account/register";
+    },function(){
+    });
+    $(".layui-layer-close1").remove();
+  }
+});
